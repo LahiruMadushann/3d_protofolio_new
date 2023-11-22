de6ba905c36bd843ee9fd27ee5ddce216b11de75
LahiruMadushann <lahirumadushandl@gmail.com>
2023-11-22 16:11:29 +0530

    Create Sky

--- 3d_portofolio_new/src/pages/Home.jsx
+++ 3d_portofolio_new/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -33,6 +34,7 @@ const Home = () => {
           <directionalLight position={[1,1,1]} intensity={2}/>
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' />
+          <Sky />
           <Island 
             position={islandPosition}
             scale={islandScale}
