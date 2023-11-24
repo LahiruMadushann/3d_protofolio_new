fa0697bff05ab267a70cdef84b4e593baaee16a9
LahiruMadushann <lahirumadushandl@gmail.com>
2023-11-24 06:51:45 +0530

    Create Alert

--- 3d_portofolio_new/src/pages/Contact.jsx
+++ 3d_portofolio_new/src/pages/Contact.jsx
@@ -4,6 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import Fox from '../models/Fox'
 import Loader from '../components/Loader'
 import useAlert from '../hooks/useAlert'
+import Alert from '../components/Alert'
 
 const Contact = () => {
   const formRef = useRef(null)
