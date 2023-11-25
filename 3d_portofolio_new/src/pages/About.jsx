0229b41bc83cd82d35d2d5153b7582a306cf7fb5
LahiruMadushann <lahirumadushandl@gmail.com>
2023-11-25 06:16:11 +0530

    Create About Section

--- 3d_portofolio_new/src/pages/About.jsx
+++ 3d_portofolio_new/src/pages/About.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow-sm'>Lahiru</span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Computer Sceince Student in Sri Lanka, specializing in technical edication through hands-on learning and building applications.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My SKills</h3>
+      </div>
+    </section>
   )
 }
 
